86e704d5e75b0be015205595203aa31b723e2f72
eliz121 <maria.mendoza03@epn.edu.ec>
2024-07-16 10:01:05 -0500

    "Bug Busters, se añadieron métodos en la clase inventario"

--- src/main/java/BL/Almacenamiento/Historial.java
+++
@@ -1,5 +0,0 @@
-package BL.Almacenamiento;
-
-public class Historial {
-    
-}
--- src/main/java/BL/Almacenamiento/Inventario.java
+++ src/main/java/BL/Almacenamiento/Inventario.java
@@ -1,9 +1,83 @@
 package BL.Almacenamiento;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import BL.GestionPaquete.Paquete;
+
 /**
  *
- * @author TOMMY
+ * @author BugsBusters
  */
+
 public class Inventario {
-    
+    private double capacidadTotal;
+    private double capacidadOcupada;
+    private int tiempoMaximo;
+    private String agencia;
+
+    public Inventario(int tiempoMaximo, double capacidadTotal,double capacidadOcupada ){
+        this.tiempoMaximo = tiempoMaximo;
+        this.capacidadTotal = capacidadTotal;
+        this.capacidadOcupada = capacidadOcupada;
+        this.agencia = "Quito";
+        //this.paquetes = new HashMap<>();
+        //this.historial = new Historial();
+    }
+
+        //Metodo que registra un paquete que se ha recibido la agencia
+
+         //Metodo que actualiza la Capacidad Ocupada del Inventario
+    public void actualizarBodega(double cantidad){
+        capacidadOcupada += cantidad;
+    }
+
+    public void actualizarCapacidadTotal(double nuevaCapacidad){
+        this.capacidadTotal = nuevaCapacidad;
+    }
+
+    // Calcular fecha límite
+
+    public double getCapacidadTotal(){
+        return capacidadTotal;
+    }
+
+    public double getCapacidadOcupada(){
+        return capacidadOcupada;
+    }
+
+    //Mostrar paquete
+
+    //Buscar paquete
+
+    private void notificarCapacidadCompleta(){
+        System.out.println("Capacidad total alcanzada");
+    }
+
+    //Retirar paquete
+
+    private String getFecha(){
+        return LocalDate.now().toString();
+    }
+    private String getHora(){
+        return LocalTime.now().toString();
+    }
+
+    private String getAgencia(){
+        return agencia;
+    }
+
+   /*  public void mostrarHistorial() {
+        historial.consultarHistorial();
+    }
+
+    public void mostrarHistorialIngresos() {
+        historial.consultarHistorialIngreso();
+    }
+
+    public void mostrarHistorialSalidas() {
+        historial.consultarHistorialSalida();
+    }
+        */
 }
