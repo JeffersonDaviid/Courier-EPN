d63bfdcbdfb760d06e5883ffcb6231ea7e109f06
dakookie <118641371+dakookie@users.noreply.github.com>
2024-07-18 12:48:53 -0500

    Maistros de Software, Metodos CamionCarga

--- src/main/java/BL/Transporte/Vehiculo.java
+++ src/main/java/BL/Transporte/Vehiculo.java
@@ -121,6 +121,8 @@ public class Vehiculo {
         return paquetes;
     }
 
+
+
     // Método para cambiar el estado del paquete en BD
     public void cambiarEstadoPaquete(String estadoInicial, String estadoFinal) {
         int rs = -1;
@@ -149,28 +151,4 @@ public class Vehiculo {
             JOptionPane.showMessageDialog(null, "El vehículo no está disponible");
         }
     }*/
-
-    // Método para listar los vehículos según la disponibilidad en la BD
-    public List<Vehiculo> listarVehiculos(int disponibilidad) {
-        List<Vehiculo> vehiculos = new ArrayList<>();
-        String sql = "SELECT * FROM Camiones WHERE disponibilidad = " + disponibilidad;
-        try {
-            DataHelper dataHelper = DataHelper.getInstancia();
-            ResultSet rs = dataHelper.executeQueryRead(sql);
-
-            while (rs.next()) {
-                
-                String modelo = rs.getString("modelo");
-                String marca = rs.getString("marca");
-                int capacidadCarga = rs.getInt("capacidad_carga");
-                int rutaId = rs.getInt("ruta_id");
-
-                Vehiculo vehiculo = new Vehiculo(modelo, marca, capacidadCarga, disponibilidad, null,rutaId);
-                vehiculos.add(vehiculo);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return vehiculos;
-    }
 }
