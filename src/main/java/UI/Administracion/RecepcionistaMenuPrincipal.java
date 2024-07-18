3df0645d27853b2ff2386d559953cefb80b90b11
Estefano Mateo Proaño Ingavelez <estefano.proano@epn.edu.ec>
2024-07-17 23:28:14 -0500

    Ventana de gestion

--- src/main/java/UI/Administracion/RecepcionistaMenuPrincipal.java
+++ src/main/java/UI/Administracion/RecepcionistaMenuPrincipal.java
@@ -6,8 +6,8 @@
 package UI.Administracion;
 
 import UI.Administracion.views.*;
-import UI.Almacenamiento.Almacenamiento;
-import UI.GestionPaquete.GestionPaqueteUI;
+import UI.GestionPaquete.GestionPaquete;
+import UI.GestionPaquete.GestionPaquete;
 import UI.SeguimientoPaquete.UIEnvio;
 import UI.SeguimientoPaquete.UISeguimiento;
 
@@ -235,7 +235,7 @@ public class RecepcionistaMenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_CerrarActionPerformed
 
     private void btn_prinActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btn_prinActionPerformed
-        ShowJPanel(new GestionPaqueteUI());
+        ShowJPanel(new GestionPaquete());
     }// GEN-LAST:event_btn_prinActionPerformed
 
     private void btn_lendsActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btn_lendsActionPerformed
