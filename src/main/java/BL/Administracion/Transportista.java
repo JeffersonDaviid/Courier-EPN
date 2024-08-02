8014b5423150ed475bc07b2808517f13cab06a73
Aizen003 <david.quille@epn.edu.ec>
2024-08-01 21:11:14 -0500

    Create Transportista.java
    transportista

---
+++ src/main/java/BL/Administracion/Transportista.java
@@ -0,0 +1,7 @@
+package BL.Administracion;
+
+public class Transportista extends Usuario {
+    public Transportista(String nombre, String contrasena, String agencia) {
+        super(nombre, contrasena, agencia);
+    }
+}
