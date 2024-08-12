b2f366601888cd7011c3221dfb509777a9689a34
Alejololer <acarlos29112003@gmail.com>
2024-08-11 21:35:23 -0500

    Metodos para Administracion
    Metodos anticipados para llamar cuando se necesite y tenga el resto de modulos

--- src/main/java/BL/Administracion/Recepcionista.java
+++ src/main/java/BL/Administracion/Recepcionista.java
@@ -65,6 +65,10 @@ public class Recepcionista extends Perfil{
         return inventario.getPaquetesInventario();
     }
 
+    public void eliminarPaqueteInventario(Paquete paquete) {
+        inventario.eliminarPaquete(paquete);
+    }
+
     // Método para agregar un nuevo usuario a la lista
     public void agregarNuevoUsuario(String agencia, String contrasena, String rol, String cedula, String nombre, String apellido, String correo) throws ClassNotFoundException {
         if(agencia == null || contrasena == null || nombre == null || rol == null || agencia.equals("") || contrasena.equals("") || nombre.equals("") || rol.equals("")) {
