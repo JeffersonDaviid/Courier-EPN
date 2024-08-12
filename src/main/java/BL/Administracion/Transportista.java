3a4058fd42071daa1e2b935858889cc25265c353
Alejololer <acarlos29112003@gmail.com>
2024-08-12 11:12:57 -0500

    Update Transportista.java
    Logica para la entrega de paquetes con chequeo y todo

--- src/main/java/BL/Administracion/Transportista.java
+++ src/main/java/BL/Administracion/Transportista.java
@@ -1,7 +1,12 @@
 package BL.Administracion;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 import BL.GestionPaquete.Entregado;
 import BL.GestionPaquete.Paquete;
+import BL.Transporte.Camion;
 import BL.Transporte.GestorTransporte;
 
 public class Transportista extends Perfil {
@@ -11,9 +16,19 @@ public class Transportista extends Perfil {
         //TODO Auto-generated constructor stub
     }
 
-    // public ArrayList<Paquete> consultarAsignacionPaquetes(){
-    //     return GestorTransporte.getInstancia().consultarAsignacionPaquetes(this);
-    // }
+    public ArrayList<Paquete> consultarAsignacionPaquetes(){
+        return GestorTransporte.getInstancia().consultarAsignacionPaquetes(this);
+    }
+
+    public boolean verificarPaquete(Paquete paquete){
+        ArrayList<Paquete> paquetes = consultarAsignacionPaquetes();
+        for (Paquete p : paquetes) {
+            if (p.getTracking().equals(paquete.getTracking())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void registrarEntregaPaquete(){
         //Logica para llamar a EntregaPaquete
@@ -27,6 +42,10 @@ public class Transportista extends Perfil {
 
     public void entregarPaquete(String idPaquete){
         Paquete paquete = inventario.buscarPaquete(idPaquete);
+        if(!verificarPaquete(paquete)){
+            JOptionPane.showMessageDialog(null, "El paquete no se encuentra asignado a este transportista.");
+            return;
+        }
         paquete.setEstado(new Entregado(paquete));
         inventario.saveInventario();
         Camion camion = GestorTransporte.getInstancia().consultarCamionAsignado(this);
